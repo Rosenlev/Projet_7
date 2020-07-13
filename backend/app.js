76fda2658af9f46ebbaba6a36318ace644dbe09b
Lucie Grand <luciemylene.grand@gmail.com>
2020-07-13 14:21:12 +0200

    controllers and routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
+
+const postRoutes = require('./routes/post');
+
+app.use('/api/post', postRoutes);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
